f6ca217c796346384baf82271e83ef654cd0d2b3
Ericksson Faustino <ericksson.faustino@gmail.com>
2019-12-14 20:49:47 -0300

    wip 12142019

--- plugin/k8szoneaware/utils.go
+++ plugin/k8szoneaware/utils.go
@@ -1,8 +1,6 @@
 package k8szoneaware
 
 import (
-	"sync"
-
 	clog "github.com/coredns/coredns/plugin/pkg/log"
 	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -81,54 +79,11 @@ func PodsFromZones(namespace string, pods *v1.PodList, zoneName string) []string
 	IpsFromPods := make([]string, 0)
 	for _, pod := range pods.Items {
 		for _, node := range AllNodes.Items {
-			if pod.Spec.NodeName == node.GetName() && node.GetLabels()["failures-domain.beta.kubernetes.io/zone"] == zoneName {
-				clog.Info("Entrei no if")
+			if pod.Spec.NodeName == node.GetName() && node.GetLabels()["failure-domain.beta.kubernetes.io/zone"] == zoneName {
 				IpsFromPods = append(IpsFromPods, pod.Status.PodIP)
 			}
 		}
 	}
-	return IpsFromPods
-}
-
-func PodsFromZoness(namespace string, pods *v1.PodList, zoneName string, k8sClient *kubernetes.Clientset) []string {
-	concurrency := len(pods.Items)
-	podCh := make(chan *v1.Pod, concurrency)
-	processedPod := make(chan *v1.Pod, 1)
-	var processWg sync.WaitGroup
-	IpsFromPods := make([]string, 0)
-
-	for i := 0; i < concurrency; i++ {
-		go func(namespace string, pod chan *v1.Pod, processed chan *v1.Pod, zoneName string, k8sClient *kubernetes.Clientset) {
-			for pod := range podCh {
-				n, _ := k8sClient.CoreV1().Nodes().Get(pod.Spec.NodeName, metav1.GetOptions{})
-				if n.GetLabels()["failure-domain.beta.kubernetes.io/zone"] == zoneName {
-					processedPod <- pod
-				} else {
-					processedPod <- nil
-				}
-			}
-		}(namespace, podCh, processedPod, zoneName, k8sClient)
-	}
-
-	go func(processed chan *v1.Pod, wg *sync.WaitGroup) {
-		for p := range processedPod {
-			if p == nil {
-				wg.Done()
-			} else {
-				IpsFromPods = append(IpsFromPods, p.Status.PodIP)
-				wg.Done()
-			}
-		}
-	}(processedPod, &processWg)
-
-	for _, pod := range pods.Items {
-		processWg.Add(1)
-		podCh <- &pod
-	}
-	processWg.Wait()
-
-	clog.Info("IPS RETORNADOS")
-	clog.Info(IpsFromPods)
 	return IpsFromPods
 }
 
